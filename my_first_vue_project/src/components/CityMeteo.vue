8abbcc24c2331804abb73ce636e89130be9eaa40
cha-d-campus <cd.webdesign.graphism@gmail.com>
2022-04-22 17:02:30 +0200

    Vue.js - First-project -first-commit

---
+++ my_first_vue_project/src/components/CityMeteo.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h3>Nom de la ville</h3>
+    <p>{{ name }}</p>
+
+    <h3>Météo</h3>
+    <ul>
+      <li>Temps : {{ weather }}</li>
+      <li>Température : {{ temperature }}</li>
+    </ul>
+
+    <h3>Date</h3>
+    <p>{{ updatedAt.toLocaleString() }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CityMeteo",
+//   props: {
+    
+//   },
+  data() {
+      return {
+        name: "Grenoble", // nom de la ville
+        weather: "Peu nuageux", // descriptif météo
+        temperature: 20.55, // température en °C
+        updatedAt: new Date(), // date de dernière mise à jour
+      };
+    },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  /* display: inline-block; */
+  margin: 15px 15px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ my_first_vue_project/src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+
+<script>
+import CityMeteo from './CityMeteo.vue'
+
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  components: {
+    CityMeteo
+  }
+};
+</script>
